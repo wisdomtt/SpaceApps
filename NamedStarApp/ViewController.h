d88ad51392353838335be1689cc9292b2fbd3582
wisdomtt <curtismichaelmason@gmail.com>
2016-09-18 11:50:21 -0400

    Create ViewController.h

---
+++ NamedStarApp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  StarSpaceApps
+//
+//  Created by Curtis Mason on 9/17/16.
+//  Copyright © 2016 Curtis Mason. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
