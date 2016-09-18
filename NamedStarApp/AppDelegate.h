423ff8ea8227bae861a90b61c48bd52e15564f27
wisdomtt <curtismichaelmason@gmail.com>
2016-09-18 11:50:43 -0400

    Create AppDelegate.h

---
+++ NamedStarApp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  StarSpaceApps
+//
+//  Created by Curtis Mason on 9/17/16.
+//  Copyright © 2016 Curtis Mason. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
